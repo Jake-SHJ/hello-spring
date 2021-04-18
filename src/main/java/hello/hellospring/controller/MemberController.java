cd9905c4cc1102fe9a0cbe628de16c0f1fb0d0b4
jake <guswns1853@gmail.com>
2021-04-18 10:59:44 +0900

    feat: Spring Bean Configuration via Java Code

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -7,10 +7,19 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MemberController {
 
+    // 생성자 주입 방식
     private final MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+//    setter 주입 방식, public으로 노출되는 것이 문제
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
+
+//    @Autowired private MemberService memberService; 필드 주입 방식
 }
